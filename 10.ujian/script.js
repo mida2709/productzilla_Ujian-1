e98c3c51b0f912c225c62ffaf359c393c232008d
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-03-17 21:06:11 +0700

    Update script.js

--- 10.ujian/script.js
+++ 10.ujian/script.js
@@ -1,15 +1,17 @@
 function tahunKabisat(tahun) {
   if (tahun % 4 === 0) {
     console.log('Kabisat');
+    
   } else if (tahun % 4 === 100) {
     console.log('Kabisat');
+    
   } else if (tahun % 4 === 400) {
     console.log('Kabisat');
+    
   } else {
     console.log('Bukan Kabisat');
   }
 }
-
 tahunKabisat(1992);
 tahunKabisat(1990);
 tahunKabisat(2100);
